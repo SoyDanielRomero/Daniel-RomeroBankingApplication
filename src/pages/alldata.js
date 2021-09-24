1e39b227be53f81710fdf8f3020fe9d3c7b12e1a
Daniel Romero <daniromerro@gmail.com>
2021-09-23 19:24:29 -0500

    Table element added

--- src/pages/alldata.js
+++ src/pages/alldata.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from '../components/card';
 import { UserContext } from '../context';
+import { Table, Container } from 'react-bootstrap';
 
 function AllData() {
   const ctx = React.useContext(UserContext);
@@ -12,28 +13,35 @@ function AllData() {
         title='Review all users data'
         text=''
         body={
-          <table className='table table-condensed'>
-            <thead>
-              <tr>
-                <th scope='col'>#</th>
-                <th scope='col'>Name</th>
-                <th scope='col'>Email</th>
-                <th scope='col'>Password</th>
-                <th scope='col'>Balance</th>
-              </tr>
-            </thead>
-            <tbody>
-              {ctx.users.map((item, key) => (
-                <tr key={key}>
-                  <th scope='row'>{key + 1}</th>
-                  <td>{item.name}</td>
-                  <td>{item.email}</td>
-                  <td>{item.password}</td>
-                  <td>{item.balance}</td>
+          <Container className='mt-3'>
+            <Table
+              className='table table-condensed'
+              striped
+              bordered
+              hover
+              size='sm'>
+              <thead>
+                <tr>
+                  <th scope='col'>#</th>
+                  <th scope='col'>Name</th>
+                  <th scope='col'>Email</th>
+                  <th scope='col'>Password</th>
+                  <th scope='col'>Balance</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {ctx.users.map((item, key) => (
+                  <tr key={key}>
+                    <th scope='row'>{key + 1}</th>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.password}</td>
+                    <td>{item.balance}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Container>
         }
       />
     </>
